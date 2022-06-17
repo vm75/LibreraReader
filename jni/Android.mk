0ea6f3ab4b31969ed9005540679deb40e94d539c
vm75 <vm75.dev@gmail.com>
2022-06-16 22:42:44 -0700

    Simplify jni build

---
+++ jni/Android.mk
@@ -0,0 +1,51 @@
+TOP_LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+ifdef BETA
+MUPDF_VERSION := 1.20
+else
+MUPDF_VERSION := 1.11
+endif
+
+ANTIWORD_ROOT := $(TOP_LOCAL_PATH)/3rdparty/antiword
+DJVU_ROOT := $(TOP_LOCAL_PATH)/3rdparty/djvu
+HQX_ROOT := $(TOP_LOCAL_PATH)/3rdparty/hqx
+LIBMOBI_ROOT := $(TOP_LOCAL_PATH)/3rdparty/libmobi
+LIBWEBP_ROOT := $(TOP_LOCAL_PATH)/3rdparty/libwebp
+MUPDF_ROOT := $(TOP_LOCAL_PATH)/3rdparty/mupdf-$(MUPDF_VERSION)
+
+include $(TOP_LOCAL_PATH)/3rdparty/antiword/Android.mk
+include $(TOP_LOCAL_PATH)/3rdparty/djvu/Android.mk
+include $(TOP_LOCAL_PATH)/3rdparty/hqx/Android.mk
+include $(TOP_LOCAL_PATH)/3rdparty/libmobi/Android.mk
+include $(TOP_LOCAL_PATH)/3rdparty/mupdf-$(MUPDF_VERSION)/Android.mk
+include $(CLEAR_VARS)
+
+LOCAL_ARM_MODE := $(MY_ARM_MODE)
+
+LOCAL_ARM_NEON := true
+
+LOCAL_C_INCLUDES := \
+	$(DJVU_ROOT)/libdjvu \
+	$(HQX_ROOT)/src \
+	$(LIBMOBI_ROOT)/src \
+	$(LIBMOBI_ROOT)/tools \
+	$(MUPDF_ROOT)/include \
+	$(MUPDF_ROOT)/source/fitz \
+	$(MUPDF_ROOT)/source/pdf \
+	$(TOP_LOCAL_PATH)/src
+
+LOCAL_CFLAGS := -DHAVE_ANDROID
+LOCAL_MODULE := Librera
+
+LOCAL_SRC_FILES := \
+	$(TOP_LOCAL_PATH)/src/cbdroidbridge.c \
+	$(TOP_LOCAL_PATH)/src/djvuDroidBridge.cpp \
+	$(TOP_LOCAL_PATH)/src/ebookdroidjni.c \
+	$(TOP_LOCAL_PATH)/src/jni_concurrent-$(MUPDF_VERSION).c \
+	$(TOP_LOCAL_PATH)/src/libmupdf-$(MUPDF_VERSION).c
+
+LOCAL_STATIC_LIBRARIES := djvu hqx mupdf_java
+LOCAL_LDLIBS := -lm -llog -ljnigraphics
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ jni/build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+SCRIPT=$(realpath "${0}")
+SCRIPT_DIR=$(dirname "${SCRIPT}")
+PROJECT_ROOT=$(dirname "${SCRIPT_DIR}")
+JNI_DIR=${PROJECT_ROOT}/jni
+APP_DIR=${PROJECT_ROOT}/app
+
+applyPatch() {
+    module=$1
+    cd ${module}
+    git reset --hard HEAD
+    git clean -f -d
+    git apply ../${module}.patch
+    cd -
+}
+
+main() {
+    cd "${PROJECT_ROOT}"
+
+    cd "${JNI_DIR}/3rdparty"
+    for module in djvu hqx libmobi libwebp ; do
+        git submodule update --init --recursive ${module}
+        applyPatch ${module}
+        if [[ ${module} =~ mupdf.* && ! -d ${module}/generate ]] ; then
+            make generate
+        fi
+
+        applyPatch ${patch}
+    done
+
+    if [[ $1 == beta ]] ; then
+        mupdf=mupdf-1.20
+        beta="BETA=1"
+    else
+        mupdf=mupdf-1.11
+    fi
+
+    applyPatch ${mupdf}
+    git submodule update --init --recursive ${mupdf}
+    if [[ ! -d ${mupdf}/generate ]] ; then
+        cd ${mupdf}
+        make generate
+        cd -
+    fi
+
+    rm -f "${APP_DIR}/src/main/jniLibs/*"
+    cd "${JNI_DIR}"
+
+    ndk-build ${beta} NDK_LIBS_OUT="${APP_DIR}/src/main/jniLibs" NDK_OUT="${JNI_DIR}/obj"
+}
+
+main "$@"
